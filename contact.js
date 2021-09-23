07f62dad0b7eae9c3da179c8d500a12f348d2925
victor-chukwu <okenwa1993@gmail.com>
2021-09-23 10:55:24 +0100

    Edited style.css and contat js

--- contact.js
+++ contact.js
@@ -3,8 +3,8 @@ const contact = document.querySelector('.contact');
 const form = document.querySelector('#book-form');
 const bookList = document.querySelector('#book-list');
 const listLink = document.querySelector('.nav-link');
-// const contactLink = document.querySelector('.link-contact');
-// const formLink = document.querySelector('.link-form');
+const contactLink = document.querySelector('.link-contact');
+const formLink = document.querySelector('.link-form');
 
 listLink.addEventListener('click', () => {
   contact.classList.add('no-show');
@@ -15,28 +15,28 @@ listLink.addEventListener('click', () => {
   bookList.classList.remove('no-show');
 });
 
-// contactLink.addEventListener('click', () => {
-//   bookList.classList.add('no-show');
-//   bookList.classList.remove('show');
-//   form.classList.add('no-show');
-//   form.classList.remove('show');
-//   contact.classList.add('show');
-//   contact.classList.remove('no-show');
-// });
+contactLink.addEventListener('click', () => {
+  bookList.classList.add('no-show');
+  bookList.classList.remove('show');
+  form.classList.add('no-show');
+  form.classList.remove('show');
+  contact.classList.add('show');
+  contact.classList.remove('no-show');
+});
 
-// formLink.addEventListener('click', () => {
-//   bookList.classList.add('no-show');
-//   bookList.classList.remove('show');
-//   form.classList.add('show');
-//   contact.classList.add('no-show');
-//   contact.classList.remove('show');
-//   form.classList.remove('no-show');
-// });
+formLink.addEventListener('click', () => {
+  bookList.classList.add('no-show');
+  bookList.classList.remove('show');
+  form.classList.add('show');
+  contact.classList.add('no-show');
+  contact.classList.remove('show');
+  form.classList.remove('no-show');
+});
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   bookList.classList.add('show');
-//   // eslint-disable-next-line no-undef
-//   const { DateTime } = luxon;
-//   this.today = DateTime.now();
-//   document.getElementById('time').textContent = this.today.toLocaleString(DateTime.DATETIME_MED);
-// });
+window.addEventListener('DOMContentLoaded', () => {
+  bookList.classList.add('show');
+  // eslint-disable-next-line no-undef
+  const { DateTime } = luxon;
+  this.today = DateTime.now();
+  document.getElementById('time').textContent = this.today.toLocaleString(DateTime.DATETIME_MED);
+});
